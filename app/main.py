dbe148720a9960b69f15af093e85263e79c8dc72
Mahin Ahmad <9mahin.ahmad@gmail.com>
2026-01-14 21:03:08 +0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 import sys
 
 
+def search_in_ospath(st):
+    import os,stat
+    dirlist = os.environ["PATH"].split(os.pathsep) # if the PATH is set to /dir1:/dir2:/dir3
+    for eachdir in dirlist:
+        # list the files in eachdir and find the matching name
+        # print(f"looking in {eachdir}")
+        # os.listdir(eachdir)
+        with os.scandir(eachdir) as scandir_iterable:  # for d in os.scandir(eachdir): directory handler stays open until garbage collection if loop ends early
+            for i in scandir_iterable:
+                if i.is_file() and i.name == st and i.stat().st_mode & stat.S_IXUSR:
+                    return eachdir + "/" + i.name # the path of the executable
+                else:
+                    continue # keep looking for executable
+            # if os.access(path, os.X_OK): # alternate way to check if a path is executable...
+    return False
+
+
 def main():
 
 
@@ -10,10 +27,12 @@ def main():
         # usercmd = sys.stdin.readline() # extra newline
 
         # match input(): # unusual behaviour, requires 2 newline and exit case invalid
-        match usercmd.split():
+        match usercmd.split():  # https://docs.python.org/3/reference/compound_stmts.html#the-match-statement
             case ['type',*rest]:
                 if rest[0] in ['echo', 'exit', 'type']:
                     sys.stdout.write(rest[0] + " is a shell builtin\n") 
+                elif (r:= search_in_ospath(rest[0])):
+                    sys.stdout.write(rest[0] + " is " + r + "\n") 
                 else:
                     sys.stdout.write(rest[0] + ": not found\n") 
             case ['echo',*rest]:
@@ -23,6 +42,7 @@ def main():
             case _:
                 sys.stdout.write(usercmd + ": command not found\n")
 
-
 if __name__ == "__main__":
     main()
+
+ 
