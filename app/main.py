1317408e4a316132d04e0d74cfc16a244a803b58
Mahin Ahmad <9mahin.ahmad@gmail.com>
2026-01-14 13:59:21 +0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ def main():
         # usercmd = sys.stdin.readline() # extra newline
 
         # match input(): # unusual behaviour, requires 2 newline and exit case invalid
-        match usercmd:
-            case 'exit':
+        match usercmd.split():
+            case ['echo',*rest]:
+                sys.stdout.write(" ".join(rest) + "\n")
+            case ['exit']:
                 sys.exit()
             case _:
                 sys.stdout.write(usercmd + ": command not found\n")
