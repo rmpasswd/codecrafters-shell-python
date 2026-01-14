efd847ec656c8d18cae6c858b4130f38d496fe63
Mahin Ahmad <35218856+rmpasswd@users.noreply.github.com>
2026-01-14 13:08:36 +0600

    message for invalid command

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    usercmd = sys.stdin.readline()
+    sys.stdout.write(f"{usercmd}: command not found")
 
 
 if __name__ == "__main__":
