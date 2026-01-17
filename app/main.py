233fb82df5ce8788de99e53e0b97647fea4b6ab9
Mahin Ahmad <9mahin.ahmad@gmail.com>
2026-01-17 11:27:07 +0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-import sys
-
+import sys, os
+import stat
 
 def search_in_ospath(st):
-    import os,stat
+    # import os,stat
     dirlist = os.environ["PATH"].split(os.pathsep) # if the PATH is set to /dir1:/dir2:/dir3
     for eachdir in dirlist:
         # list the files in eachdir and find the matching name
@@ -11,7 +11,7 @@ def search_in_ospath(st):
         try:
             with os.scandir(eachdir) as scandir_iterable:  # for d in os.scandir(eachdir): directory handler stays open until garbage collection if loop ends early
                 for i in scandir_iterable:
-                    if i.is_file() and i.name == st and i.stat().st_mode & stat.S_IXUSR:
+                    if i.name == st  and  i.is_file()  and i.stat().st_mode & stat.S_IXUSR: # or just use convenient fun: shutil.which and shutil.is_executable()
                         return eachdir + "/" + i.name # the path of the executable
                     else:
                         continue # keep looking for executable
@@ -31,7 +31,7 @@ def main():
         usercmd = input()
         # usercmd = sys.stdin.readline() # extra newline
 
-        # match input(): # unusual behaviour, requires 2 newline and exit case invalid
+        # match input(): # unusual behaviour, requires 2 newline and 'exit case' is invalid
         match usercmd.split():  # https://docs.python.org/3/reference/compound_stmts.html#the-match-statement
             case ['type',*rest]:
                 if rest[0] in ['echo', 'exit', 'type']:
@@ -44,10 +44,21 @@ def main():
                 sys.stdout.write(" ".join(rest) + "\n")
             case ['exit']:
                 sys.exit()
+            case [othercmd, *rest]:
+                os.execlp(othercmd,*rest)
             case _:
+                try:
+                    # Task is to :
+                    # For example, if the user types custom_exe arg1 arg2, your shell should:
+                    # Execute it with three arguments: custom_exe (the program name), arg1, and arg2
+                    # [cmd, *args] = usercmd.split()
+                    os.execvp(usercmd.split()[0], usercmd.split())
+                except:
+                    pass # probably could not find the command in PATH
                 sys.stdout.write(usercmd + ": command not found\n")
 
 if __name__ == "__main__":
     main()
 
  
+
